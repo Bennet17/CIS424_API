87277cb2b10bcfc3a86d788a31a67c29b4128550
Tabal <daniel.hansen0516@yahoo.com>
2024-01-31 20:26:04 -0500

    Created all models according to schema as of 1/31/24

---
+++ CIS424_API/Models/Register.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CIS424_API.Models
+{
+    public class Register
+    {
+        public int registerID { get; set; }
+        public int storeID { get; set; }
+        public int amount { get; set; }
+    }
+}
