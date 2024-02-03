9aaa9d5b6d88776259ef93e496cec33e1a19422f
hwii23 <hjwright@svsu.edu>
2024-02-03 14:40:49 -0500

    Added controllers

--- CIS424_API/Controllers/AuthenticateUserController.cs
+++ CIS424_API/Controllers/AuthenticateUserController.cs
@@ -24,6 +24,7 @@ namespace CIS424_API.Controllers
         {
 
             string connectionString = "Server=tcp:capsstone-server-01.database.windows.net,1433;Initial Catalog=capstone_db_01;Persist Security Info=False;User ID=SA_Admin;Password=Capstone424!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            
             try
             {
                 using (SqlConnection connection = new SqlConnection(connectionString))
