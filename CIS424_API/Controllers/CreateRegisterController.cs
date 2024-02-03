c25d724b8721feed5df40452476a1ae056f3a9ac
hwii23 <hjwright@svsu.edu>
2024-02-03 14:49:50 -0500

    Readded store model to create register route

--- CIS424_API/Controllers/CreateRegisterController.cs
+++ CIS424_API/Controllers/CreateRegisterController.cs
@@ -20,7 +20,7 @@ namespace CIS424_API.Controllers
         // POST SVSU_CIS424/CreateRegister
         [HttpPost]
         [Route("CreateRegister")]
-        public IHttpActionResult CreateRegister([FromBody])
+        public IHttpActionResult CreateRegister([FromBody] Store store)
         {
             string connectionString = "Server=tcp:capsstone-server-01.database.windows.net,1433;Initial Catalog=capstone_db_01;Persist Security Info=False;User ID=SA_Admin;Password=Capstone424!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
