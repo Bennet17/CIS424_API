6cd163baebe4d7ed63d6a8584cd51f076bf41f5e
Gwalden <70655924+mediocre251@users.noreply.github.com>
2024-02-13 08:56:01 -0500

    Update EnableRegisterController.cs

--- CIS424_API/Controllers/EnableRegisterController.cs
+++ CIS424_API/Controllers/EnableRegisterController.cs
@@ -36,7 +36,7 @@ namespace CIS424_API.Controllers
                     {
                         command.CommandType = CommandType.StoredProcedure;
 
-                        command.Parameters.AddWithValue("@ID", register.RegisterID);
+                        command.Parameters.AddWithValue("@ID", register.registerID);
 
                         SqlParameter resultMessageParam = new SqlParameter("@ResultMessage", SqlDbType.VarChar, 255);
                         resultMessageParam.Direction = ParameterDirection.Output;
