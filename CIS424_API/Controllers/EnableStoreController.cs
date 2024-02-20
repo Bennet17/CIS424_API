44bd68fc7ca1b17c0768a7f83a6614adc701e07d
Tabal <daniel.hansen0516@yahoo.com>
2024-02-20 12:52:26 -0500

    Reformatted EnableStore and EnableUser arguements and changed HttpPost from HttpGet

--- CIS424_API/Controllers/EnableStoreController.cs
+++ CIS424_API/Controllers/EnableStoreController.cs
@@ -16,12 +16,12 @@ namespace CIS424_API.Controllers
     [EnableCors(origins: "http://localhost:3000", headers: "*", methods: "*")]
     public class EnableStoreController : ApiController
     {
-        // GET SVSU_CIS424/ViewStores
-        // Returns a list of all stores in the database
-        [HttpGet]
+        // POST SVSU_CIS424/EnableStore
+        // Enables a store in the database
+        [HttpPost]
         [Route("EnableStore")]
 
-        public IHttpActionResult Post([FromBody] Store store)
+        public IHttpActionResult Post([FromBody] int storeID)
         {
             string connectionString = "Server=tcp:capsstone-server-01.database.windows.net,1433;Initial Catalog=capstone_db_01;Persist Security Info=False;User ID=SA_Admin;Password=Capstone424!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
@@ -35,7 +35,7 @@ namespace CIS424_API.Controllers
                     {
                         command.CommandType = CommandType.StoredProcedure;
 
-                        command.Parameters.AddWithValue("@ID", store.ID);
+                        command.Parameters.AddWithValue("@ID", storeID);
 
                         SqlParameter resultMessageParam = new SqlParameter("@ResultMessage", SqlDbType.VarChar, 255);
                         resultMessageParam.Direction = ParameterDirection.Output;
