f0d91b7fc9be4855154368bda8dc7187d71056fa
Tabal <daniel.hansen0516@yahoo.com>
2024-02-20 11:22:32 -0500

    Changed GET Request for ViewUsersByStoreId parameters to FromUri from FromBody.

--- CIS424_API/Controllers/ViewUsersController.cs
+++ CIS424_API/Controllers/ViewUsersController.cs
@@ -63,7 +63,7 @@ namespace CIS424_API.Controllers
         // Returns a list of all users in the database for a store by the storeID
         [HttpGet]
         [Route("ViewUsersByStoreID")]
-        public IHttpActionResult GetUsersByStoreID([FromBody] User storeID)
+        public IHttpActionResult GetUsersByStoreID([FromUri] int storeID)
         {
             string connectionString = "Server=tcp:capsstone-server-01.database.windows.net,1433;Initial Catalog=capstone_db_01;Persist Security Info=False;User ID=SA_Admin;Password=Capstone424!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
@@ -79,7 +79,7 @@ namespace CIS424_API.Controllers
                         command.CommandType = CommandType.StoredProcedure;
 
                         // Add parameter for the stored procedure.
-                        command.Parameters.AddWithValue("@storeID", storeID.storeID);
+                        command.Parameters.AddWithValue("@storeID", storeID);
 
                         // Create a SqlDataReader object
                         using (SqlDataReader reader = command.ExecuteReader())
