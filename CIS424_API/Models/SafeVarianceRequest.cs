1b639caccaabf1a88ddd2bde2be4d0303bab1681
John <bennet17@gmail.com>
2024-03-05 12:00:46 -0500

    Added Variance models and controller

---
+++ CIS424_API/Models/SafeVarianceRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CIS424_API.Models
+{
+    public class SafeVarianceRequest
+    {
+        public int storeID { get; set; }
+        public string startDate { get; set; }
+        public string endDate { get; set; }
+    }
+}
