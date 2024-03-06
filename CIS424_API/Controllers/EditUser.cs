df39b7dc230450b12a5028aaa95dadfe3e7e5bfc
Blue RiverFlow <43072021+dlfegan@users.noreply.github.com>
2024-03-06 18:47:48 -0500

    changed employeeID to usrID in cashCount and fundTransfer, and edited store.cs,EditUser controller added

---
+++ CIS424_API/Controllers/EditUser.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using CIS424_API.Models;
+
+// EditUser Controller route
+namespace CIS424_API.Controllers
+{
+    [RoutePrefix("SVSU_CIS424")]
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class EditUserController : ApiController
+    {
+        // POST SVSU_CIS424/EditUser
+        // Creates a user by storeID in the database
+        [HttpPost]
+        [Route("EditUser")]
+        public IHttpActionResult EditUser([FromBody] User user)
+        {
+            string connectionString = "Server=tcp:capsstone-server-01.database.windows.net,1433;Initial Catalog=capstone_db_01;Persist Security Info=False;User ID=SA_Admin;Password=Capstone424!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Create a SqlCommand object for the stored procedure.
+                    using (SqlCommand command = new SqlCommand("sp_EditUser", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        // Add parameters for the stored procedure.
+                        command.Parameters.AddWithValue("@name", user.name);
+                        command.Parameters.AddWithValue("@storeID",user.storeID);
+                        command.Parameters.AddWithValue("@usrID",user.usrID);
+                        command.Parameters.AddWithValue("@username",user.username);
+                        command.Parameters.AddWithValue("@position",user.position);
+
+
+                        // Add output parameter
+                        SqlParameter resultMessageParam = new SqlParameter("@ResultMessage", SqlDbType.VarChar, 255);
+                        resultMessageParam.Direction = ParameterDirection.Output;
+                        command.Parameters.Add(resultMessageParam);
+
+                        // Execute the stored procedure
+                        command.ExecuteNonQuery();
+
+                        // Retrieve the result message
+                        string resultMessage = resultMessageParam.Value.ToString();
+
+
+                        // Return the response as JSON object.
+                        return Ok(new { response = resultMessage });
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that may occur during database operations.
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
