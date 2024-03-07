9217404073a93d19a53f1c609c4454e38078ffc9
Blue RiverFlow <43072021+dlfegan@users.noreply.github.com>
2024-03-06 19:29:20 -0500

    EditUser modified

--- CIS424_API/Controllers/EditUser.cs
+++ CIS424_API/Controllers/EditUser.cs
@@ -15,7 +15,7 @@ namespace CIS424_API.Controllers
         // POST SVSU_CIS424/EditUser
         [HttpPost]
         [Route("EditUser")]
-        public IHttpActionResult EditUser([FromBody] User user)
+        public IHttpActionResult EditUser([FromBody] EditUser user)
         {
             string connectionString = "Server=tcp:capsstone-server-01.database.windows.net,1433;Initial Catalog=capstone_db_01;Persist Security Info=False;User ID=SA_Admin;Password=Capstone424!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
