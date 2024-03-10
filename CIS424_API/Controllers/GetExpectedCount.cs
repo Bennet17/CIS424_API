08d5998ef2f1ca7267a75e1eb90111df593fab32
SpyroNew <157664855+SpyroNew@users.noreply.github.com>
2024-03-10 01:51:28 -0500

    Fixed for efficiency added getCloseCount for safe

--- CIS424_API/Controllers/GetExpectedCount.cs
+++ CIS424_API/Controllers/GetExpectedCount.cs
@@ -12,10 +12,10 @@ namespace CIS424_API.Controllers
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class GetExpectedCountController : ApiController
     {
-        // POST SVSU_CIS424/GetOpenExpectedCount
+        // POST SVSU_CIS424/GetOpenCount
         [HttpGet]
-        [Route("GetExpectedCount")]
-        public IHttpActionResult GetExpectedCount([FromUri] int usrID, [FromUri] String itemCounted)
+        [Route("GetOpenCount")]
+        public IHttpActionResult GetOpenCount([FromUri] int storeID, [FromUri] int registerID)
         {
             string connectionString = "Server=tcp:capsstone-server-01.database.windows.net,1433;Initial Catalog=capstone_db_01;Persist Security Info=False;User ID=SA_Admin;Password=Capstone424!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
@@ -28,8 +28,48 @@ namespace CIS424_API.Controllers
                     using (SqlCommand command = new SqlCommand("sp_GetOpenExpectedAmount", connection))
                     {
                         command.CommandType = CommandType.StoredProcedure;
-                        command.Parameters.AddWithValue("@usrID", usrID);
-                        command.Parameters.AddWithValue("@itemCounted", itemCounted);
+                        command.Parameters.AddWithValue("@storeID", storeID);
+                        command.Parameters.AddWithValue("@register", registerID);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                decimal expectedAmount = reader.GetDecimal(0); // Assuming the result is a decimal value
+                                return Ok(expectedAmount);
+                            }
+                            else
+                            {
+                                // No rows returned
+                                return NotFound();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that may occur during database operations.
+                return InternalServerError(ex);
+            }
+        }
+
+         [HttpGet]
+        [Route("GetCloseCount")]
+        public IHttpActionResult GetCloseCount([FromUri] int storeID)
+        {
+            string connectionString = "Server=tcp:capsstone-server-01.database.windows.net,1433;Initial Catalog=capstone_db_01;Persist Security Info=False;User ID=SA_Admin;Password=Capstone424!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand("sp_GetCloseExpectedAmount", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@storeID", storeID);
 
                         using (SqlDataReader reader = command.ExecuteReader())
                         {
