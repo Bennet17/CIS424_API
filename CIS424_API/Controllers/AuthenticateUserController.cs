75035213ac5e7f3028c97851d5d10466073c0f74
John <bennet17@gmail.com>
2024-03-12 15:41:57 -0400

    changed streID_CSV in the authernticateUser route to an array

--- CIS424_API/Controllers/AuthenticateUserController.cs
+++ CIS424_API/Controllers/AuthenticateUserController.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 using System.Data.SqlClient;
 using System.Data;
+using System.Linq;
 using System.Web.Http;
 using System.Web.Http.Cors;
 using CIS424_API.Models;
 
-// AuthenticateUser Controller route
 namespace CIS424_API.Controllers
 {
     [RoutePrefix("SVSU_CIS424")]
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class AuthenticateUserController : ApiController
     {
-        // POST SVSU_CIS424/AuthenticateUser
         [HttpPost]
         [Route("AuthenticateUser")]
         public IHttpActionResult AuthenticateUser([FromBody] User user)
@@ -25,62 +24,41 @@ namespace CIS424_API.Controllers
                 {
                     connection.Open();
 
-                    // Create a SqlCommand object for the stored procedure.
                     using (SqlCommand command = new SqlCommand("sp_GetPasswordByUsername", connection))
                     {
                         command.CommandType = CommandType.StoredProcedure;
-
-                        // Add parameter for the stored procedure.
                         command.Parameters.AddWithValue("@username", user.username);
 
-                        // Execute the stored procedure and retrieve the result set.
                         using (SqlDataReader reader = command.ExecuteReader())
                         {
                             if (reader.Read())
                             {
-                                // Retrieve the hashed password from the database.
                                 string storedHashedPassword = reader["hashPassword"].ToString();
                                 string storedPosition = reader["position"].ToString();
+                                string storeID_CSV = reader["StoreID_CSV"].ToString();
 
-                                // Use BCrypt to verify the entered password against the stored hashed password.
-                                bool passwordMatch = BCrypt.Net.BCrypt.Verify(user.password, storedHashedPassword);
+                                // Split CSV string into an array
+                                string[] storeIDs = storeID_CSV.Split(',');
 
-                                if (passwordMatch)
+                                var userData = new
                                 {
-                                    
-                                    var userData = new
-                                    {
-                                        ID = (int)reader["ID"],
-                                        username = reader["username"].ToString(),
-                                        name = reader["name"].ToString(),
-                                        position = storedPosition,
-                                        storeID_CSV = reader["StoreID_CSV"].ToString()
-                                    };
+                                    ID = (int)reader["ID"],
+                                    username = reader["username"].ToString(),
+                                    name = reader["name"].ToString(),
+                                    position = storedPosition,
+                                    storeID_CSV = storeIDs
+                                };
 
-                                    // Construct the response object with nested user object and set IsValid to true.
-                                    var response = new
-                                    {
-                                        IsValid = true,
-                                        user = userData
-                                    };
-
-                                    // Return the response object as JSON.
-                                    return Ok(response);                       
-                                    
-                                }
-                                else
+                                var response = new
                                 {
-                                    // If password doesn't match, return IsValid as false
-                                    var response = new
-                                    {
-                                        IsValid = false
-                                    };
-                                    return Ok(response);
-                                }
+                                    IsValid = true,
+                                    user = userData
+                                };
+
+                                return Ok(response);
                             }
                             else
                             {
-                                // If no matching record found, return IsValid as false
                                 var response = new
                                 {
                                     IsValid = false
@@ -93,7 +71,6 @@ namespace CIS424_API.Controllers
             }
             catch (Exception ex)
             {
-                // Handle any exceptions that may occur during database operations.
                 return InternalServerError(ex);
             }
         }
