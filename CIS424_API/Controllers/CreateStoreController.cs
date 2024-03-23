d82d3f9dbf6c6a1e3ff9cd8d018efc037b08bcbc
gwaldenb <110639045+gwaldenb@users.noreply.github.com>
2024-03-23 12:03:46 -0400

    changed the controllers which hadn't already been changed to work off of the const created by john which contains the connectionstring.

--- CIS424_API/Controllers/CreateStoreController.cs
+++ CIS424_API/Controllers/CreateStoreController.cs
@@ -18,11 +18,11 @@ namespace CIS424_API.Controllers
         [Route("CreateStore")]
         public IHttpActionResult CreateStore([FromBody] Store store)
         {
-            string connectionString = "Server=tcp:capsstone-server-01.database.windows.net,1433;Initial Catalog=capstone_db_01;Persist Security Info=False;User ID=SA_Admin;Password=Capstone424!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            //string connectionString = "Server=tcp:capsstone-server-01.database.windows.net,1433;Initial Catalog=capstone_db_01;Persist Security Info=False;User ID=SA_Admin;Password=Capstone424!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
             try
             {
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                using (SqlConnection connection = new SqlConnection(ConnectionString.SQL_Conn))
                 {
                     connection.Open();
 
