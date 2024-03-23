853ce4920be02a96048f33585697ebdbb66d72c4
John <bennet17@gmail.com>
2024-03-23 14:18:55 -0400

    Added the code for authenication of the routes, created a basecontroller for this. All controllers will need to inherit from this base controller, the logic to call the method in basecontoller is commented out in the ViewUsersController, need to coordinate with frontend on when to uncomment it out add it to all of the routes.

---
+++ CIS424_API/Controllers/BaseApiController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+public class BaseApiController : ApiController
+{
+    protected const string SharedKey = "password";
+
+    protected bool AuthenticateRequest(HttpRequestMessage request)
+    {
+        if (request.Headers.TryGetValues("API_Key", out var headerValues))
+        {
+            var apiKey = headerValues.FirstOrDefault();
+            if (apiKey == SharedKey)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
