98122ebbd6be49c28f50b869be9f49d2cec1ab35
Tabal <daniel.hansen0516@yahoo.com>
2024-04-02 11:43:19 -0400

    Added alias to Register model and controllers using the register model. Removed the use of create register. Create Register route now allows for an alias. Create Register now accounts for null values as an alias. ViewRegisterByStoreID now accounts for alias.

--- CIS424_API/Controllers/ViewStoresController.cs
+++ CIS424_API/Controllers/ViewStoresController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Data;
 using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Cors;
 using CIS424_API.Models;
